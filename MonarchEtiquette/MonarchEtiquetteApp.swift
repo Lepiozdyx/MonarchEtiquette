868b75a8d0be74ef3292eceeeec2ac90d91c9f1e
Developer <Lepiozdyx@gmail.com>
2026-02-23 13:07:23 -0300

    Initial Commit

---
+++ MonarchEtiquette/MonarchEtiquetteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonarchEtiquetteApp.swift
+//  MonarchEtiquette
+//
+//  Created by Alex on 23.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct MonarchEtiquetteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
